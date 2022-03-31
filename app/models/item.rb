cff3ecd37c410810ce4afebc4fd118f1bda24f9a
ngtspc <cube1090@gmail.com>
2022-03-31 14:16:44 +0300

    Callbacks

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :name, presence: true
+
+  after_initialize { p 'initialize' } # Item.new Item.first
+  after_save       { p 'save' } # Item.create Item.save
+  after_create     { p 'create' } # Item.create
+  after_update     { p 'update' } # Item.update
+  after_destroy    { p 'destroy' } #Item.last Item.destroy
+
 end
