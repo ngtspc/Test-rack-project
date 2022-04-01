a37448ae738b7584ab71956cd5b96ca4748ba66d
ngtspc <cube1090@gmail.com>
2022-04-01 17:11:27 +0300

    Params and saving to a database

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
-
+  layout false
+  skip_before_action :verify_authenticity_token
   def index
     @items = Item.all
     render body: @items.map { |i| "#{i.name}:#{i.price}:" }
+  end
+
+  def create
+    item = Item.create(items_params)
+    if item.persisted?
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :price, :real, :weight)
   end
 end
